e984c42bffba3848ac562f8ef09c7b3cdd1fe553
Aryan Dadhich <Lovelydadhich143@gmail.com>
2022-04-07 13:05:39 +0530

    day 11 completed

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -4,20 +4,21 @@ import 'package:flutter/material.dart';
 class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final imageUrl =
+    const imageUrl =
         "https://media-exp1.licdn.com/dms/image/C4E03AQHeXZboMjd93w/profile-displayphoto-shrink_200_200/0/1639501131395?e=1651708800&v=beta&t=MUW3jxYpYwp3aMoOMpl2V8AJPt5azzNnyk9M2mchk9g";
     return Drawer(
       child: Container(
         color: Colors.deepPurple,
         child: ListView(
           padding: EdgeInsets.zero,
+          // ignore: prefer_const_literals_to_create_immutables
           children: [
-            DrawerHeader(
+            const DrawerHeader(
               padding: EdgeInsets.zero,
               child: UserAccountsDrawerHeader(
                 margin: EdgeInsets.zero,
-                accountName: const Text("Aryan Dadhich"),
-                accountEmail: const Text("rajkumardadheech6@gmail.com"),
+                accountName: Text("Aryan Dadhich"),
+                accountEmail: Text("rajkumardadheech6@gmail.com"),
                 currentAccountPicture: CircleAvatar(
                   backgroundImage: NetworkImage(imageUrl),
                 ),
